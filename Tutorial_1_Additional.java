812ffde6ec0d68dd759b4e20bc92140fbb1e2729
Jababeanflic <62802478+Jababeanflic@users.noreply.github.com>
2020-09-13 18:29:11 +0100

    Updated

--- Tutorial_1_Additional.java
+++ Tutorial_1_Additional.java
@@ -2,33 +2,33 @@ import java.util.Scanner;
 
 public class Tutorial_1_Additional {
 
-    static Scanner kboard = new Scanner(System.in);
+    static Scanner kboard = new Scanner(System.in); // Scanner used in multiple methods
 
-    public static void statistic(int gamesWon, int gamesPlayed) {
+    public static void statistic(int gamesWon, int gamesPlayed) { // Method to calculate win percent
 
-        System.out.println("Games played: "+gamesPlayed);
-        System.out.println("Games won: "+gamesWon);
-        int winPercent = gamesWon*100/gamesPlayed;
-        System.out.println("Percentage of games won: " +winPercent+ "%");
-    }
+        System.out.println("Games played: " + gamesPlayed);
+        System.out.println("Games won: " + gamesWon);
+        int winPercent = gamesWon * 100 / gamesPlayed;
+        System.out.println("Percentage of games won: " + winPercent + "%");
+    }// end method
 
-    public static void totalCards(int[][] cardsDrawn) {
+    public static void totalCards(int[][] cardsDrawn) { // Method to print out each card and the number of times it was drawn
 
         System.out.println("Frequency of each card drawn");
 
-        for (int i = 1; i < cardsDrawn.length; i++) {
+        for (int i = 1; i < cardsDrawn.length; i++) {// loops number of times equal to length of cards drawn array so 10
             System.out.println("Number of time card number " + i + " was drawn: " + cardsDrawn[i][0]);
         }
         System.out.println();
-    }
+    }// end method
 
-    public static int getRandomNumber() {
+    public static int getRandomNumber() { // Method generates random number from 1-10
         return ((int) (Math.random() * 10) + 1);
-    }
+    }// end method
 
-    public static int getRandomNumberDealer() {
+    public static int getRandomNumberDealer() {// Method generates random number for dealer from 16-21 (modify to draw Cards)
         return ((int) (Math.random() * (6) + 16));
-    }
+    }// end method
 
     public static int[][] cardsDrawn(int[] cardsDrawn, int[][] totalCardsDrawn) // Method to store which cards were drawn
     {
@@ -40,12 +40,12 @@ public class Tutorial_1_Additional {
                 else {
                     System.out.print("," + cardsDrawn[x]);
                 }
-            totalCardsDrawn[cardsDrawn[x]][0] = totalCardsDrawn[cardsDrawn[x]][0] + 1; // add new card to total cards drawn count
-        }
+            totalCardsDrawn[cardsDrawn[x]][0] = totalCardsDrawn[cardsDrawn[x]][0] + 1;
+        } // add new card to total cards drawn count so x=the card that was drawn and +1 is added to the "sub array"
 
         System.out.println();
-        return (totalCardsDrawn);
-    }
+        return (totalCardsDrawn); // Returns the total card drawn array within array, first index is card number second
+    } // end method               // index is the number of time that card was drawn
 
     public static void main(String[] args) {
 
@@ -87,10 +87,10 @@ public class Tutorial_1_Additional {
                     System.out.println("Your bust!");
                     totalCards = cardsDrawn(cardsDrawn, totalCardsDrawn);
                     break; // Ends the game and program
-                }
+                }// end if
                 System.out.println("Do you want to draw another card y/n?");
                 choice = kboard.next();
-            }
+            } // end while
             if (choice.equalsIgnoreCase("n")) {
                 if (total > dealer) {
                     System.out.println("You have won with a total of " + total + " Dealer has " + dealer);
@@ -99,23 +99,23 @@ public class Tutorial_1_Additional {
                     System.out.println("You have lost with a total of " + total + " Dealer has " + dealer);
                 }
                 totalCards = cardsDrawn(cardsDrawn, totalCardsDrawn);
-            }
+            }// end if
             if (gamesPlayed < 5) {
                 System.out.println("Number of games played " + gamesPlayed + ", do you want to play again y/n?");
                 choice = kboard.next();
                 if (choice.equalsIgnoreCase("y")) { // loop to play another game and record games played
                     gamesPlayed++;
                     PlayGame = true; // set to true to remain in do/while loop
-                }
+                }// end if
             } else {
                 System.out.println("You have played " + gamesPlayed + " games thats the max, Bye!");
                 break;
-            }
-        } while (PlayGame);
+            }// end if/else
+        } while (PlayGame);// Game will loop if boolean PlayGame is true
 
-        statistic(gamesWon, gamesPlayed); // call method statistics to calculate win percent pass gamesWon and games played total
-        totalCards(totalCards); //call method total number of cards drawn for all games and pass array of cards already drawn
-    }
-}
+        statistic(gamesWon, gamesPlayed);//call method statistics to calculate win percent pass gamesWon and games played total
+        totalCards(totalCards);//call method total number of cards drawn for all games and pass array of cards already drawn
+    }// end main method
+}// end class
 
 
